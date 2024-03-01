c436fe73ea1ae7cb2192b278c71ee18ea359115f
Veres Zoltán <zoltan.veres@nokia.com>
2024-03-01 11:44:31 +0200

    Add Robot Framework :: Tool to classifiers

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
                  'Programming Language :: Python :: 3.10',
                  'Programming Language :: Python :: 3.11',
                  'Programming Language :: Python :: 3.12',
+                 'Framework :: Robot Framework :: Tool',
                  'Topic :: Software Development'],
     keywords='robotframework',
     url='https://github.com/nokia/crl-rfcli',
